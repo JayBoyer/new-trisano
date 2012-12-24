csinitiative/trisano

Repeater refactor, objects not parms munging

webapp/db/migrate/20121129184222_remove_repeater_parent_key_from_core_field.rb
@@ -1,0 +1,9 @@
+class RemoveRepeaterParentKeyFromCoreField < ActiveRecord::Migration
+  def self.up
+    remove_column :core_field, :repeater_parent_key
+  end
+
+  def self.down
+    add_column :core_fields, :repeater_parent_key, :string
+  end
+end

JayBoyer/new-trisano

webapp/db/migrate/20121129184222_remove_repeater_parent_key_from_core_field.rb
@@ -1,0 +1,9 @@
+class RemoveRepeaterParentKeyFromCoreField < ActiveRecord::Migration
+  def self.up
+    remove_column :core_fields, :repeater_parent_key
+  end
+
+  def self.down
+    add_column :core_fields, :repeater_parent_key, :string
+  end
+end