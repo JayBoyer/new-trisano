6d64380ede8dee87b5926ad4bfaa421dd829ec52
JayBoyer <boyer@snhdmail.org>
2013-05-28 14:02:56 -0700

    change session store to use active_record in db, instead of cookie

--- webapp/config/environment.rb
+++ webapp/config/environment.rb
@@ -80,7 +80,7 @@ Rails::Initializer.run do |config|
   # If you change this key, all old sessions will become invalid!
   # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
-  config.action_controller.session_store = :cookie_store
+  # config.action_controller.session_store = :cookie_store
 
 
   # In order to avoid publishing the session secret to a repo,
@@ -98,7 +98,7 @@ Rails::Initializer.run do |config|
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
-  # config.action_controller.session_store = :active_record_store
+  config.action_controller.session_store = :active_record_store
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
