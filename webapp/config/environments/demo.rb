87ea754d637daa29ae519c5e82a5495796628720
Jay Boyer <boyer@snhdmail.org>
2014-02-10 13:58:01 -0800

    add demo deployment

---
+++ webapp/config/environments/demo.rb
@@ -0,0 +1,71 @@
+# Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013 The Collaborative Software Foundation
+#
+# This file is part of TriSano.
+#
+# TriSano is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# TriSano is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with TriSano. If not, see http://www.gnu.org/licenses/agpl-3.0.txt.
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The test environment is used exclusively to run your application's
+# test suite.  You never need to work with it otherwise.  Remember that
+# your test database is "scratch space" for the test suite and is wiped
+# and recreated between test runs.  Don't rely on the data there!
+config.cache_classes = true
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
+
+# Disable request forgery protection in test environment
+config.action_controller.allow_forgery_protection    = false
+
+# Tell ActionMailer not to deliver emails to the real world.
+# The :test delivery method accumulates sent emails in the
+# ActionMailer::Base.deliveries array.
+config.action_mailer.delivery_method = :test
+
+require 'logging'
+
+# Logging.init is required to avoid 
+#   unknown level was given 'info' (ArgumentError)
+# or
+#   uninitialized constant Logging::MAX_LEVEL_LENGTH (NameError)
+# when an Appender or Layout is created BEFORE any Logger is instantiated:
+Logging.init :debug, :info, :warn, :error, :fatal
+
+# see https://github.com/TwP/logging/blob/master/lib/logging/layouts/pattern.rb
+# for pattern formatting reference
+layout = Logging::Layouts::Pattern.new :pattern => "[%p] [%d] [%-5l] %m\n"
+
+# Default logfile, history kept for 10 days
+TRISANO_LOG_LOCATION = ENV['TRISANO_LOG_LOCATION'] ||= '/var/log/trisano/'
+if TRISANO_LOG_LOCATION.split('').last != '/'
+  TRISANO_LOG_LOCATION = TRISANO_LOG_LOCATION + '/'
+end
+default_appender = Logging::Appenders::RollingFile.new 'default', :filename => TRISANO_LOG_LOCATION + 'trisano.log', :age => 'daily', :keep => 10, :safe => true, :layout => layout
+
+DEFAULT_LOGGER = Logging::Logger['server']
+DEFAULT_LOGGER.add_appenders default_appender
+if ENV['TRISANO_LOG_LEVEL'] != nil
+  DEFAULT_LOGGER.level = ENV['TRISANO_LOG_LEVEL'].intern
+else
+  DEFAULT_LOGGER.level = :info
+end
+
+config.logger = DEFAULT_LOGGER
+
+#make translation fail loud
+#require 'trisano/i18n/fail_fast'
