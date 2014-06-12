df5baa4fadadfd2bbacddad7f456ca49003bada5
Jay Boyer <boyer@snhdmail.org>
2014-06-12 10:54:38 -0700

    remove unused old firefox version

--- webapp/features/support/firefox-36/defaults/pref/firefox-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 36 "/builds/slave/rel-m-192-lnx-bld/build/browser/locales/en-US/firefox-l10n.js"
-
-//@line 38 "/builds/slave/rel-m-192-lnx-bld/build/browser/locales/en-US/firefox-l10n.js"
-
-pref("general.useragent.locale", "en-US");
