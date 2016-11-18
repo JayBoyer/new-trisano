fcee328ae4d42db5a6eb980c3c6a50d1a6809973
Jay Boyer <boyer@snhdmail.org>
2016-11-18 14:36:06 -0800

    enhancements pdf generation

--- webapp/app/controllers/generate_pdf_controller.rb
+++ webapp/app/controllers/generate_pdf_controller.rb
@@ -27,6 +27,83 @@ class GeneratePdfController < ApplicationController
         end
       end
       
+      #get the list of STD conditions
+      diseases = Disease.find_by_sql("Select d.disease_name from diseases d " +
+        "INNER join disease_events de on de.disease_id = d.id AND de.event_id = " + event_id)
+      disease_name = "STD/HIV Co-Infection"
+      if(!diseases.blank? && !diseases[0].blank?)
+        disease_name = diseases[0].disease_name.strip
+      end
+      
+      disease_to_code = { 
+        "AIDS Adult" => "950",
+        "AIDS Pediatric"=> "950",
+        "Chlamydia trachomatis infection" => "200",
+        "Gonococcal Infection" => "300",
+        "HIV Infection, adult" => "900",
+        "HIV Infection, pediatric" => "900",
+        "HIV pediatric seroreverter" => "900",
+        "HIV perinatal exposure" => "900",
+        "Syphilis" => "700",
+        "Syphilis, congenital" => "790",
+        "Syphilis, early latent" => "730",
+        "Syphilis, late latent" => "745",
+        "Syphilis, late with clinical manifestations other than neurosyphilis" => "750",
+        "Syphilis, neurosyphilis" => "700",
+        "Syphilis, primary" => "710",
+        "Syphilis, secondary" => "720",
+        "Syphilis, unknown latent" => "700"
+      }
+      
+      # check for multiple conditions
+      std_condition_values = []
+      form_short_name = "STD_field_record"
+      form_coinfect = "std_coinfection"
+      if (event.type == "ContactEvent")
+        form_short_name = "STD_contact_field_record"
+        form_coinfect = "std_contact_coinfection"
+      end
+      answer = fetch_answers(:first, event_id, form_short_name, "ref_basis")
+      ref_basis = (!answer.blank? ? answer.text_answer : '')
+      answer = fetch_answers(:first, event_id, form_short_name, "init_dt")
+      init_dt = (!answer.blank? ? answer.text_answer : '')
+      if(disease_name.include?("Co-Infection"))
+        
+        keys = [
+          ["ct_condition", "ct_dispo"],
+          ["gc_condition", "gc_dispo"],
+          ["syph_condition", "syph_dispo"],
+          ["hiv_condition", "hiv_dispo"],
+        ]
+        keys.each do |key|
+          values = []
+          values[0] = ref_basis
+          answer = fetch_answers(:first, event_id, form_coinfect, key[0])
+          disease_name = (!answer.blank? ? answer.text_answer : '').strip
+          if(!disease_name.blank?)
+            if(disease_name.length > 2)
+              code = disease_name.slice(0,3)
+              if (code == 'Yes')
+                code = (key[0] == 'ct_condition')  ? '200' : '300'
+              end
+              values[1] = code
+              values[2] = init_dt
+              answer = fetch_answers(:first, event_id, form_short_name, key[1])
+              values[3] = (!answer.blank? ? answer.text_answer : '')
+              std_condition_values.push(values)
+            end
+          end
+        end
+      else
+        values = []
+        values[0] = ref_basis
+        values[1] = disease_to_code[disease_name]
+        values[2] = init_dt
+        answer = fetch_answers(:first, event_id, form_short_name, "dispo")
+        values[3] = (!answer.blank? ? answer.text_answer : '')
+        std_condition_values[0] = values
+      end
+      
       pdf_path = Rails.root.to_s + "/app/pdfs/"
       template = pdf_path + "template/" + template_pdf_name + "_template.pdf"
 
@@ -71,12 +148,7 @@ class GeneratePdfController < ApplicationController
           if(mapping.operation == "multi_line")
             which = :all
           end
-          answers = Answer.find(which, :conditions => ["answers.event_id = ?", event_id], :joins=> 
-            "INNER JOIN form_references fr ON fr.event_id = " + event_id + 
-            " INNER JOIN forms f ON f.id = fr.form_id AND f.short_name = '" + mapping['form_short_name'] + "'" +
-            " INNER JOIN form_elements fe ON fe.form_id = f.id " +
-            " INNER JOIN questions q ON q.form_element_id = fe.id AND q.short_name = '" + mapping['form_field_name'] + "'" +
-            " AND q.id = answers.question_id", :order => "id ASC")
+          answers = fetch_answers(which, event_id, mapping['form_short_name'], mapping['form_field_name'])
           if (answers.is_a?(Array))
             if(answers.length > 0)
               values = []
@@ -140,7 +212,7 @@ class GeneratePdfController < ApplicationController
           if(output_fields[mapping['template_field_name']].blank?)
             output_fields[mapping['template_field_name']] = values[0]
           end
-        when "replace_if_src_not_blank"
+        when /replace_if_src_not_blank|answer_code/
           if(!values[0].blank?)
             output_fields[mapping['template_field_name']] = values[0]
           end
@@ -163,8 +235,21 @@ class GeneratePdfController < ApplicationController
           
           lab_results = LabResult.find_by_sql(sql)
           lab_results.each do |lab_result|
-            output_fields[mapping['template_field_name']] += "Test type: " + lab_result['test_type'] + 
-                ", Result: " + lab_result['result'] + "," + lab_result['result_value'] + ", Collection date: " + lab_result['collection_date'].to_s + "\n"
+            date = lab_result['collection_date']
+            collection_date = ''
+            if(!date.blank?) 
+              # fix screwed up collection dates
+              year = date.year
+              month = date.mon
+              day = date.day
+              if(date.year < 1900)
+                collection_date = (2000+day).to_s + "-" + month.to_s.rjust(2, "0") + "-" + year.to_s.rjust(2, "0")
+              else
+                collection_date = year.to_s + "-" + month.to_s,rjust(2, "0") + "-" + day.to_s.rjust(2, "0")
+              end
+            end
+           output_fields[mapping['template_field_name']] += "Test type: " + lab_result['test_type'] + 
+                ", Result: " + lab_result['result'] + "," + lab_result['result_value'] + ", Collection date: " + collection_date + "\n"
           end
         when "treatments"
           sql = "SELECT COALESCE(to_char(pt.treatment_date, 'DD-MM-YY'), '') as treatment_date, COALESCE(t.treatment_name, '') as treatment_name " +
@@ -178,13 +263,11 @@ class GeneratePdfController < ApplicationController
             output_fields[mapping['template_field_name']] += treatment['treatment_date'].to_s + ": " + treatment['treatment_name'] + "\n"
           end
         when "std_condition_code"
-          # don't replace if field is already set
-          if (output_fields[mapping['template_field_name']].blank? && !values[0].blank? && values[0].length > 2)
-            result = values[0].slice(0,3)
-            if (result == 'Yes')
-              result = (mapping['template_field_name'] == 'ct_condition')  ? '200' : '300'
-            end
-            output_fields[mapping['template_field_name']] = result
+          indices = mapping['match_value'].split(",")
+          i = indices[0].to_i
+          j = indices[1].to_i
+          if(std_condition_values.length >= i)
+            output_fields[mapping['template_field_name']] = std_condition_values[i-1][j-1]
           end
         else # treat as replace
           output_fields[mapping['template_field_name']] = values[0]
@@ -218,6 +301,15 @@ class GeneratePdfController < ApplicationController
         send_file(pdf_filename, :filename => template_pdf_name + event_id + '.pdf', :type => "application/pdf")
       end
     end
+  end
+  
+  def fetch_answers(which, event_id, form_short_name, form_field_name)
+    return Answer.find(which, :conditions => ["answers.event_id = ?", event_id], :joins=> 
+      "INNER JOIN form_references fr ON fr.event_id = " + event_id + 
+      " INNER JOIN forms f ON f.id = fr.form_id AND f.short_name = '" + form_short_name + "'" +
+      " INNER JOIN form_elements fe ON fe.form_id = f.id " +
+      " INNER JOIN questions q ON q.form_element_id = fe.id AND q.short_name = '" + form_field_name + "'" +
+      " AND q.id = answers.question_id", :order => "id ASC")
   end
 end
   
--- webapp/app/pdfs/template/ooj_template.sql
+++ webapp/app/pdfs/template/ooj_template.sql
@@ -13,17 +13,23 @@ INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_s
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'first_name', 'core', 'interested_party|person_entity|person|first_name', NULL, NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'middle_initial', 'core', 'interested_party|person_entity|person|middle_name', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'middle_name', 'core', 'interested_party|person_entity|person|middle_name', NULL, NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'aka', 'STD_core_elements', 'aka', NULL, NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'date_of_birth', 'core', 'interested_party|person_entity|person|birth_date', NULL, '-', 'date_m_d_y', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'age', 'core', 'interested_party|person_entity|person|approximate_age_no_birthday', NULL, NULL, NULL, NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'age', '', '', NULL, NULL, 'age', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'birth_gender', 'core', 'interested_party|person_entity|person|birth_gender_id', 'gender', NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'current_gender', 'STD_contact_field_record', 'current_gender', NULL, NULL, 'answer_code', NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'current_gender', 'STD_core_elements', 'current_gender', NULL, NULL, 'answer_code', NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'marital_status', 'STD_contact_field_record', 'marital_status', NULL, NULL, 'answer_code', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'marital_status', 'STD_core_elements', 'marital_status', NULL, NULL, 'answer_code', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
@@ -31,13 +37,21 @@ INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_s
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'race', 'core', 'interested_party|person_entity|race_ids', 'race', NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'height', 'std_physical_desc', 'height', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'height', 'STD_contact_field_record', 'height', NULL, NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'size', 'std_physical_desc', 'size', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'height', 'std_physical_desc', 'height', NULL, NULL, 'replace_if_src_not_blank', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'complexion', 'std_physical_desc', 'complexion', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'size', 'STD_contact_field_record', 'size', NULL, NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hair', 'std_physical_desc', 'hair', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'size', 'std_physical_desc', 'size', NULL, NULL, 'replace_if_src_not_blank', NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'complexion', 'STD_contact_field_record', 'complexion', NULL, NULL, NULL, NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'complexion', 'std_physical_desc', 'complexion', NULL, NULL, 'replace_if_src_not_blank', NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'hair', 'STD_contact_field_record', 'hair', NULL, NULL, NULL, NULL);
+INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
+  VALUES ('ooj', 'hair', 'std_physical_desc', 'hair', NULL, NULL, 'replace_if_src_not_blank', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'other_physical_desc', 'std_physical_desc', 'other_physical_desc', NULL, NULL, NULL, NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value)
@@ -81,57 +95,37 @@ INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_s
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'full_name', 'core', 'interested_party|person_entity|person|last_name', NULL, ' ', 'concat', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'referral_basis', 'STD_field_record', 'ref_basis', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'referral_basis_1', '', '', NULL, NULL, 'std_condition_code', '1,1');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'referral_basis', 'STD_contact_field_record', 'ref_basis', NULL, NULL, 'replace_if_dst_blank', NULL);
+  VALUES ('ooj', 'std_condition_1', '', '', NULL, NULL, 'std_condition_code', '1,2');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ct_dispo', 'STD_field_record', 'ct_dispo', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'init_date_1', '', '', NULL, NULL, 'std_condition_code', '1,3');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ct_dispo', 'STD_contact_field_record', 'ct_dispo', NULL, NULL, 'replace_if_dst_blank', NULL);
+  VALUES ('ooj', 'std_dispo_1', '', '', NULL, NULL, 'std_condition_code', '1,4');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ct_condition', 'std_coinfection', 'ct_condition', NULL, NULL, 'std_condition_code', NULL);
+  VALUES ('ooj', 'referral_basis_2', '', '', NULL, NULL, 'std_condition_code', '2,1');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ct_condition', 'std_contact_coinfection', 'ct_condition', NULL, NULL, 'std_condition_code', NULL);
+  VALUES ('ooj', 'std_condition_2', '', '', NULL, NULL, 'std_condition_code', '2,2');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ct_dispo_dt', 'STD_field_record', 'ct_dispo_dt', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'init_date_2', '', '', NULL, NULL, 'std_condition_code', '2,3');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ct_dispo_dt', 'STD_contact_field_record', 'ct_dispo_dt', NULL, NULL, 'replace_if_dst_blank', NULL);
+  VALUES ('ooj', 'std_dispo_2', '', '', NULL, NULL, 'std_condition_code', '2,4');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ng_dispo', 'STD_field_record', 'ng_dispo', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'referral_basis_3', '', '', NULL, NULL, 'std_condition_code', '3,1');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ng_dispo', 'STD_contact_field_record', 'ng_dispo', NULL, NULL, 'replace_if_dst_blank', NULL);
+  VALUES ('ooj', 'std_condition_3', '', '', NULL, NULL, 'std_condition_code', '3,2');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ng_condition', 'std_coinfection', 'ng_condition', NULL, NULL, 'std_condition_code', NULL);
+  VALUES ('ooj', 'init_date_3', '', '', NULL, NULL, 'std_condition_code', '3,3');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ng_condition', 'std_contact_coinfection', 'ng_condition', NULL, NULL, 'std_condition_code', NULL);
+  VALUES ('ooj', 'std_dispo_3', '', '', NULL, NULL, 'std_condition_code', '3,4');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ng_dispo_dt', 'STD_field_record', 'ng_dispo_dt', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'referral_basis_4', '', '', NULL, NULL, 'std_condition_code', '4,1');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'ng_dispo_dt', 'STD_contact_field_record', 'ng_dispo_dt', NULL, NULL, 'replace_if_dst_blank', NULL);
+  VALUES ('ooj', 'std_condition_4', '', '', NULL, NULL, 'std_condition_code', '4,2');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'syph_dispo', 'STD_field_record', 'syph_dispo', NULL, NULL, NULL, NULL);
+  VALUES ('ooj', 'init_date_4', '', '', NULL, NULL, 'std_condition_code', '4,3');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'syph_dispo', 'STD_contact_field_record', 'syph_dispo', NULL, NULL, 'replace_if_dst_blank', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'syph_condition', 'std_coinfection', 'syph_condition', NULL, NULL, 'std_condition_code', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'syph_condition', 'std_contact_coinfection', 'syph_condition', NULL, NULL, 'std_condition_code', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'syph_dispo_dt', 'STD_field_record', 'syph_dispo_dt', NULL, NULL, NULL, NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'syph_dispo_dt', 'STD_contact_field_record', 'syph_dispo_dt', NULL, NULL, 'replace_if_dst_blank', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hiv_dispo', 'STD_field_record', 'hiv_dispo', NULL, NULL, NULL, NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hiv_dispo', 'STD_contact_field_record', 'hiv_dispo', NULL, NULL, 'replace_if_dst_blank', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hiv_condition', 'std_coinfection', 'hiv_condition', NULL, NULL, 'std_condition_code', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hiv_condition', 'std_contact_coinfection', 'hiv_condition', NULL, NULL, 'std_condition_code', NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hiv_dispo_dt', 'STD_field_record', 'hiv_dispo_dt', NULL, NULL, NULL, NULL);
-INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
-  VALUES ('ooj', 'hiv_dispo_dt', 'STD_contact_field_record', 'hiv_dispo_dt', NULL, NULL, 'replace_if_dst_blank', NULL);
+  VALUES ('ooj', 'std_dispo_4', '', '', NULL, NULL, 'std_condition_code', '4,4');
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
   VALUES ('ooj', 'lab_results', '', '', NULL, NULL, 'lab_results', NULL);
 INSERT INTO generate_pdf_mappings(template_pdf_name, template_field_name, form_short_name, form_field_name, code_name, concat_string, operation, match_value) 
